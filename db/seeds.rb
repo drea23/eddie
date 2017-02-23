8fd643d34489be6973cfda8d7a29b6a20e4431e9
Alexis Fieujean <fieujean.alexis@gmail.com>
2017-02-23 18:48:51 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 Artist.destroy_all
 Product.destroy_all
 Domain.destroy_all
